f939275c09cd4fb89d207d879ec4933531fa5569
Justin Richard Verducci <jrverducci@gmail.com>
2018-11-20 16:27:40 -0800

    Demo

--- client/src/components/trackSanta.js
+++ client/src/components/trackSanta.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import './Home.css';
 import {Button, ButtonToolbar} from 'react-bootstrap';
+import sleigh from './sleigh.png';
 
 class TrackSanta extends Component {
+  constructor(props){
+    super(props)
+
+    this.onHome = this.onHome.bind(this)
+  }
     
+  onHome(){
+    this.props.history.push('/home')
+  }
 
   render() {
     return (
-      <div className="Home">
+      <div className="Home-free">
+      <Button bsStyle="danger" bsSize="small" onClick={this.onHome}>Go Home</Button>
         <header className="Home-header">
         <h1>
             Merry Christmas
           </h1>
-          <img src="http://res.freestockphotos.biz/pictures/17/17382-illustration-of-a-red-santa-hat-pv.png" className="Home-logo" alt="logo" />
+          <img src={sleigh} className="Home-logo" alt="logo" />
           <div className="row">
           <ButtonToolbar>
           <Button bsStyle="success" bsSize="large" href='https://santatracker.google.com/'>TRACK SANTA</Button>
